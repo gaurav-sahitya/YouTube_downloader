5d11d1d47a4410d21715da7fe4d82a8f226ad5a4
gaurav <gauravsahitya108@gmail.com>
2020-08-27 21:33:59 +0530

    "added essentials"

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from pytube import YouTube
-url=input("Enter YouTube URL: ")#"https://www.youtube.com/watch?v=P6au4e5SDA8"
-yt=YouTube(url)
-print(yt.streams.filter(progressive=True,file_extension="mp4").order_by("resolution"))
+try:
+    from pytube import YouTube
+
+    url=input("Enter YouTube URL: ") #"https://www.youtube.com/watch?v=668nUCeBHyY"
+    yt=YouTube(url)
+    vid_str=yt.streams.filter(type="video",subtype="mp4").order_by("resolution").desc()
+    aud_str=yt.streams.filter(type="audio").order_by("abr").desc()
+
+    vid_qul=[str(i).split()[3].split('"')[1] for i in vid_str]
+    aud_qul=[str(i).split()[3].split('"')[1] for i in aud_str]
+
+    print("\nAVAILABLE QUALITIES:")
+    print("VIDEO:",sorted(set(vid_qul),key=lambda x:int(x[0:-1]),reverse=True))
+    print("AUDIO:",sorted(set(aud_qul),key=lambda x:int(x[0:-4]),reverse=True))
+
+    while 1:
+        qul=input("\nEnter the quality: ").lower().strip()
+
+        if qul in vid_qul:
+            print("\nVIDEO DOWNLOADED :)\nAt:",vid_str[vid_qul.index(qul)].download())
+            break
+        elif qul in aud_qul:
+            print("\nAUDIO DOWNLOADED :)\nAt:",aud_str[aud_qul.index(qul)].download())
+            break
+        else:
+            print("Check your input :(")
+except:
+    print("Something went wrong :(")
